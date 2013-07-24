d68e0542276b99de255faa26fd8072cc38afb7b2
Alina Guzman <alinochka8@gmail.com>
2013-07-24 17:44:58 -0400

    Added mouseover and mouseout opacity changes to the buttons

--- js/carousel.js
+++ js/carousel.js
@@ -10,3 +10,28 @@ carousel.style.marginLeft = 0;
 // to change their opacity from .5 to .2
 
 // Create onclick events to advance the images
+
+next.onclick = function(event){
+    // code you put in here will be run when the next button is clicked
+
+};
+
+previous.onlick = function(event){
+
+};
+
+next.onmouseover = function(event){
+    this.style.opacity = .9;
+};
+
+previous.onmouseover = function(event){
+    this.style.opacity = .9;
+};
+
+next.onmouseout = function(event){
+    this.style.opacity = .4;
+};
+
+previous.onmouseout = function(event){
+    this.style.opacity = .4;
+};
